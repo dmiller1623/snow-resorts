f635c4119c6090cae7ea6d9609fb8b931406ea5b
Dennis <dmiller1623@gmail.com>
2019-01-28 17:17:48 -0700

    Import apikey and set up fetch call

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,3 +1,6 @@
-const getWeather = () => {
-  
+import apiKey from '../utilities/apiKey.js'
+
+export const getWeather = async() => {
+  const response = await fetch(`https://api.darksky.net/forecast/${apiKey}`);
+  const weather = response.json();
 }
